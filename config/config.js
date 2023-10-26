8e6ec0a4d9b628da54cb2173aa8b8902011315de
ismail939 <ismailsalama526@gmail.com>
2023-10-26 10:50:48 +0300

    sql

---
+++ config/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+module.exports = {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        port: 3306,
+        dialect: process.env.DIALECT
+    },
+    production: {
+        username: process.env.DB_USERNAME,  
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        dialect: process.env.DIALECT,
+        logging: false
+    }
+}
