8a5d18e4f44538f123796f34042589f55b104572
Tarek-Ahmed-Helmy <tarek.ahmed35.01@gmail.com>
2023-10-26 14:02:08 +0300

    loly

--- models/index.js
+++ models/index.js
@@ -1,6 +1,17 @@
 const Sequelize = require('sequelize')
 const db = require('../config/database')
-const ClientModel = require('./Client')
+
+//const BookModel = require('./Book')
+const ClientModel = require("./Client")
+const CwspaceModel = require("./Cwspace")
+const CwspacePhoneModel = require("./CwspacePhone");
+const EventModel = require("./Event")
+const EventPhotoModel = require("./EventPhoto");
+const OwnerModel = require("./Owner")
+//const ReviewModel = require("./Client")
+const RoomModel = require("./Room")
+//const SubscribeModel = require("./Client")
+
 
 
 // create model
