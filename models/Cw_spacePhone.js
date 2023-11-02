a4e3e7369d48730c90c83051ad265cc9778ab1a1
Tarek Ahmed <109629579+Tarek-Ahmed-Helmy@users.noreply.github.com>
2023-11-02 09:45:55 +0200

    Delete models directory

--- models/Cw_spacePhone.js
+++
@@ -1,9 +0,0 @@
-module.exports = (db, type) => {
-    return db.define("cw_space_phones", {
-        phone: {
-            type: type.STRING
-        }
-    },{
-        timestamps: false 
-     })
-} 
--- models/EventPhoto.js
+++
@@ -1,9 +0,0 @@
-module.exports = (db, type) => {
-    return db.define("event_photos", {
-        photo: {
-            type: type.TEXT('tiny')
-        }
-    },{
-        timestamps: false 
-     });
-};
