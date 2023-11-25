dc7d36ad75ec7bba776b716cd0ec12895166c7c4
Tarek-Ahmed-Helmy <tarek.ahmed35.01@gmail.com>
2023-11-25 16:02:33 +0200

    functions validators

--- Back-end/utils/validators.js
+++ Back-end/utils/validators.js
@@ -7,5 +7,26 @@ const isEmail = (email) => {
     return undefined
 }
 
+const isEmpty = (attribute, text) => {
+    if (!text) return `${attribute} is required`
+    return undefined
+}
 
-console.log(isEmail('som3amasry@gmail.com'))
+const isTime = (time) => {
+    const timePattern = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+    if(!timePattern.test(time)){
+        return "not in time format"
+    }
+    return undefined
+}
+
+const isDate = (date) => {
+    const datePattern = /^\d{4}-\d{2}-\d{2}$/;
+    if (!datePattern.test(date)) {
+        return "not in date format"
+    }
+    return undefined
+}
+
+
+console.log(isDate('2022-10-hh'))
