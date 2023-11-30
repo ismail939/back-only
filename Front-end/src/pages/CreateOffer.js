438191b28b7efa90c899c4e63c56843373066913
unknown <yousef.ismail.akareb1elprince1@gmail.com>
2023-11-30 12:33:44 +0200

    date

--- Front-end/src/pages/CreateOffer.js
+++ Front-end/src/pages/CreateOffer.js
@@ -42,7 +42,7 @@ function CreateOffer() {
     }
     const addData = () => {
         if (isImage(offerImageName)) {
-            const formData={'img': img,'imageName': offerImageName,'title': title ,'description': description, 'start': start, 'end': end}
+            const formData = { 'img': img, 'imageName': offerImageName, 'title': title, 'description': description, 'start': start, 'end': end }
             // const formData = new FormData();
             // formData.append('img', img);
             // formData.append('imageName', offerImageName);
@@ -61,7 +61,7 @@ function CreateOffer() {
                 .then(response => response.json())
                 .then(data => {
                     if (data.status === "error") {
-                       
+
                         setErrorMessage(data.message);
                     } else if (data.status === "success") {
                         console.log(data);
@@ -69,11 +69,11 @@ function CreateOffer() {
                 })
                 .catch(error => {
                     console.error('Error during fetch operation:', error);
-                    
+
                 });
         }
     }
-    
+
     // const titleError= (title) => {
     //     if (title.length === 0) {
     //         return true;
@@ -213,7 +213,7 @@ function CreateOffer() {
 
                                         value={start}
                                         min={getDate()}
-                                        max="2024-11-28"
+                                        max={end?end:"2024-11-28"}
                                         className={`bg-gray-50 border ${dataerrors.start ? "border-red-500" : "border-gray-300"} text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
                                         required
                                         onChange={(e) => {
