277babf021a29bd4037538a0e3829792b89775a8
amodather <abdo.modather10@gmail.com>
2023-12-06 23:49:59 +0200

    Style updated

--- Front-end/src/components/OwnerAccountSettings.js
+++ Front-end/src/components/OwnerAccountSettings.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-function OwnerAccountSettings(props){
-    const profileData=props.profileData;
+function OwnerAccountSettings(props) {
+    const profileData = props.profileData;
     const [active, setActive] = useState("account settings");
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
@@ -25,7 +25,7 @@ function OwnerAccountSettings(props){
     const addImg = () => {
         const formData = new FormData();
         formData.append('profilePic', img);
-        fetch('http://localhost:4000/owners/addPhoto/mango', {
+        fetch('http://localhost:4000/owners/addPhoto/owner123', {
             method: 'PATCH',
             body: formData,
         })
@@ -229,94 +229,112 @@ function OwnerAccountSettings(props){
             addPassword();
         }
     }
-    return(
+    return (
         <>
             <div className="w-full min-h-screen py-1 md:w-2/3 lg:w-3/4 " >
-                    <div className="" >
-                        <div className="my-4 border  border-black-90 rounded-3xl  grid max-w-3xl mx-auto mt-8  ">
-                            <img className=" object-cover w-40 h-40 p-1 rounded-full ring-2 ring-indigo-300 m-8 " src={imageUrl} alt="no-picture-added"></img>
-                            <input className={`bg-gray-50 border mx-12 px-4  ${dataerrors.imgName ? "border-red-500" : "border-gray-300"}
+                <h2 className="max-w-3xl mx-auto mt-8 px-2 font-bold text-2xl">Profile Photo</h2>
+                <div className="my-4 border border-black-90 rounded-3xl max-w-3xl mx-auto mt-4" >
+                    <div className="w-full px-16">
+                        <img className=" object-cover w-40 h-40 p-1 rounded-full ring-2 ring-indigo-300 m-8 " src={imageUrl} alt="no-picture-added"></img>
+                        <input className={`bg-gray-50 border mx-12 px-4  ${dataerrors.imgName ? "border-red-500" : "border-gray-300"}
                         text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block max-w-2xl  p-2.5`}
-                                onChange={(e) => { setImg(e.target.files[0]); setImgName(e.target.files[0].name) }}
-                                accept=".png,.jpg,.jpeg" type="file" ></input>
-                            {dataerrors.imgName ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
-                            <button className="py-2 px-6 mx-80  my-2  text-base font-medium text-indigo-100 focus:outline-none btn-color rounded-lg border border-indigo-200 hover:bg-indigo-900 
-                            focus:z-10 focus:ring-4 focus:ring-indigo-200 " disabled={!imgName.trim()} onClick={(e) => handleImage(e)}>save</button>
+                            onChange={(e) => { setImg(e.target.files[0]); setImgName(e.target.files[0].name) }}
+                            accept=".png,.jpg,.jpeg" type="file" ></input>
+                        {dataerrors.imgName ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
+                        <div className="flex flex-row-reverse w-full">
+                            <button className="py-2 px-8 my-2  text-base font-medium text-indigo-100 focus:outline-none btn-color
+                        rounded-lg border border-indigo-200 hover:bg-indigo-900 " disabled={!imgName.trim()} onClick={(e) => handleImage(e)}>Save</button>
                         </div>
                     </div>
-                    <div className="my-4 border  border-black-90 rounded-3xl  grid max-w-3xl mx-auto mt-8">
-                        
-                        <div className="m-4">
-                            <div className="my-4" >
-                                <label className="" />First Name
-                                <input className={`bg-gray-50 border mx-7 ${dataerrors.fname ? "border-red-500" : "border-gray-300"}
-                        text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-1/2 p-2.5`}
+                </div>
+                <h2 className="max-w-3xl mx-auto mt-8 px-2 font-bold text-2xl">Basic Information</h2>
+                <div className="my-4 border border-black-90 rounded-3xl max-w-3xl mx-auto mt-4">
+                    <div className="w-full px-16">
+                        <div className="my-4 w-full flex justify-between items-center" >
+                            <label className="block mb-2 cursor-icon w-1/4 gap-2">First Name</label>
+                            <div className="w-full">
+                                <input className={`bg-gray-50 border ${dataerrors.fname ? "border-red-500" : "border-gray-300"}
+                            text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 w-full`}
                                     onChange={(e) => setFirstName(e.target.value)} type="text" placeholder={profileData.fname} ></input>
                                 {dataerrors.fname ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
                             </div>
-                            <div className="my-4">
-                                <label className="" />Last Name
-                                <input className={`bg-gray-50 border mx-7 ${dataerrors.lname ? "border-red-500" : "border-gray-300"} 
-                        text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-1/2 p-2.5`}
+                        </div>
+                        <div className="my-4 w-full flex justify-between items-center">
+                            <label className="block mb-2 cursor-icon w-1/4 gap-2">Last Name</label>
+                            <div className="w-full">
+                                <input className={`bg-gray-50 border ${dataerrors.lname ? "border-red-500" : "border-gray-300"} 
+                            text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-full p-2.5`}
                                     onChange={(e) => setLastName(e.target.value)} type="text" placeholder={profileData.lname}></input>
                                 {dataerrors.lname ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
-                            </div >
-                            <div className="my-4">
-                                <label className="" />Email
-                                <input className={`bg-gray-50 border mx-16 ${dataerrors.email ? "border-red-500" : "border-gray-300"} 
-                        text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-1/2 p-2.5`}
+                            </div>
+                        </div >
+                        <div className="my-4 w-full flex justify-between items-center">
+                            <label className="block mb-2 cursor-icon w-1/4 gap-2">Email</label>
+                            <div className="w-full">
+                                <input className={`bg-gray-50 border ${dataerrors.email ? "border-red-500" : "border-gray-300"} 
+                            text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-full p-2.5`}
                                     type="email" onChange={(e) => setEmail(e.target.value)} placeholder={profileData.email}></input>
                                 {dataerrors.email ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
                             </div>
-                            <div>
-                                <button cla disabled={!firstName.trim() && !lastName.trim() && !email.trim()}
-                                    onClick={e => HandleError(e)} className="py-2 px-7 mx-80  my-2  text-base font-medium text-indigo-100 focus:outline-none btn-color rounded-lg border border-indigo-200 hover:bg-indigo-900 
-                                    focus:z-10 focus:ring-4 focus:ring-indigo-200" >save</button>
-                            </div>
+                        </div>
+                        <div className="flex flex-row-reverse w-full">
+                            <button disabled={!firstName.trim() && !lastName.trim() && !email.trim()}
+                                onClick={e => HandleError(e)} className="py-2 px-8 my-2 text-base font-medium text-indigo-100 focus:outline-none btn-color rounded-lg border border-indigo-200 hover:bg-indigo-900 
+                                    " >Save</button>
                         </div>
                     </div>
-                    <div className="my-4 border  border-black-90 rounded-3xl  grid max-w-3xl mx-auto mt-8">
-                        <div className="m-2 p-2">
-                        
-                        <input className={`bg-gray-50 border mx-44 ${dataerrors.phonenumber ? "border-red-500" : "border-gray-300"} 
-                        text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-1/2 p-2.5`}
+                </div>
+                <h2 className="max-w-3xl mx-auto mt-8 px-2 font-bold text-2xl">PhoneNumber</h2>
+                <div className="my-4 border  border-black-90 rounded-3xl max-w-3xl mx-auto mt-4">
+                    <div className="flex justify-between items-center py-4 px-16 gap-8">
+                        <input className={`bg-gray-50 border ${dataerrors.phonenumber ? "border-red-500" : "border-gray-300"} 
+                        text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-full p-2.5`}
                             onChange={(e) => setPhoneNumber(e.target.value)} type="text"
                             placeholder={profileData.phone} ></input>
-                        {dataerrors.phonenumber ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
-                        <button className="py-2 px-7 mx-80  mt-2   text-base font-medium text-indigo-100 
-                        focus:outline-none btn-color rounded-lg border border-indigo-200 hover:bg-indigo-900 
+                        <button className="py-2 px-8 text-base font-medium text-indigo-100 w-28
+                        focus:outline-none btn-color rounded-lg border border-indigo-200 hover:bg-indigo-900  
                                     " disabled={!phoneNumber.trim()}
-                            onClick={e => handlePhone(e)} >save </button>
-                        </div>
-                        
+                            onClick={e => handlePhone(e)} >Save</button>
                     </div>
-                    <div className="my-4 border  border-black-90 rounded-3xl  grid max-w-3xl mx-auto mt-8">
-                        <div className="m-4">
-                        <div className="my-4" >
-                            <label className="mx-4" />old password
-                            <input type="password" onChange={(e) => setOldPassword(e.target.value)} className={`bg-gray-50 border ${dataerrors.oldpassword ? "border-red-500" : "border-gray-300"} 
-                        text-gray-900 mx-24 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-1/2 p-2.5`}></input>
-                            {dataerrors.oldpassword ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
-                        </div>
-                        <div className="my-4">
-                            <label className="mx-4" />new password
-                            <input type="password" onChange={(e) => setNewPassword(e.target.value)} className={`bg-gray-50 border ${dataerrors.newpassword ? "border-red-500" : "border-gray-300"} 
-                        text-gray-900 mx-20 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-1/2 p-2.5`}></input>
-                            {dataerrors.newpassword ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
-                        </div>
-                        <div className="my-4">
-                            <label className="mx-4" />confirm password
-                            <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} className={`bg-gray-50 border ${dataerrors.confrimpassword ? "border-red-500" : "border-gray-300"} 
-                        text-gray-900 mx-12 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-1/2 p-2.5`}></input>
-                            {dataerrors.confrimpassword ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
-                            {!compPassword() ? <p className="text-rose-600 text-xs mt-1 flex items-center gap-1 inline-block">Password doesn't match</p> : null}
-                        </div>
-                        <button className="py-2 px-7 mx-80  my-2  text-base font-medium text-indigo-100 focus:outline-none btn-color rounded-lg border border-indigo-200 hover:bg-indigo-900 
-                                    focus:z-10 focus:ring-4 focus:ring-indigo-200" disabled={!oldPassword.trim() && !newPassword.trim() && !confrimPassword.trim()} onClick={(e) => handlePassword(e)} >save</button>
+                    <div className="px-16 mb-3">
+                        {dataerrors.phonenumber ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
                     </div>
-                        </div>
-                        
                 </div>
+                <h2 className="max-w-3xl mx-auto mt-8 px-2 font-bold text-2xl">Passsword</h2>
+                <div className="my-4 border border-black-90 rounded-3xl max-w-3xl mx-auto mt-4">
+                    <div className="w-full px-16">
+                        <div className="my-4 w-full flex justify-between items-center gap-2" >
+                            <label className="block mb-2 cursor-icon w-1/4">Old Password</label>
+                            <div className="w-full">
+                                <input type="password" onChange={(e) => setOldPassword(e.target.value)} className={`bg-gray-50 border ${dataerrors.oldpassword ? "border-red-500" : "border-gray-300"} 
+                        text-gray-900 sm:text-sm rounded-lg w-full p-2.5`}></input>
+                                {dataerrors.oldpassword ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
+                            </div>
+                        </div>
+                        <div className="my-4 w-full flex justify-between items-center gap-2">
+                            <label className="block mb-2 cursor-icon w-1/4">New Password</label>
+                            <div className="w-full">
+                                <input type="password" onChange={(e) => setNewPassword(e.target.value)} className={`bg-gray-50 border ${dataerrors.newpassword ? "border-red-500" : "border-gray-300"} 
+                        text-gray-900 sm:text-sm rounded-lg w-full p-2.5`}></input>
+                                {dataerrors.newpassword ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
+                            </div>
+                        </div>
+                        <div className="my-4 w-full flex justify-between items-center gap-2">
+                            <label className="block mb-2 cursor-icon w-1/4">Confirm Password</label>
+                            <div className="w-full">
+                                <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} className={`bg-gray-50 border ${dataerrors.confrimpassword ? "border-red-500" : "border-gray-300"} 
+                        text-gray-900 sm:text-sm rounded-lg  w-full p-2.5`}></input>
+                                {dataerrors.confrimpassword ? <span className="text-[12px] text-red-500">{checkerror}</span> : null}
+                                {!compPassword() ? <p className="text-rose-600 text-xs mt-1 flex items-center gap-1 inline-block">Password doesn't match</p> : null}
+                            </div>
+                        </div>
+                        <div className="flex flex-row-reverse w-full">
+                            <button className="py-2 px-8 my-2  text-base font-medium text-indigo-100 focus:outline-none btn-color
+                        rounded-lg border border-indigo-200 hover:bg-indigo-900" disabled={!oldPassword.trim() && !newPassword.trim() && !confrimPassword.trim()} onClick={(e) => handlePassword(e)} >Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- Front-end/src/pages/DashboardProfile.js
+++ Front-end/src/pages/DashboardProfile.js
@@ -4,8 +4,10 @@ import OwnerAccountSettings from "../components/OwnerAccountSettings";
 function DashboardProfile() {
     const [active, setActive] = useState("account settings");
     const [profileData, setProfileData] = useState([]);
+    const buttonStyle = "w-48 font-semibold px-8 py-3 hover:border-blue-500 border rounded-2xl"
+    const activeStyle= "bg-[#0F4C75] text-white hover:bg-[#197ec2] duration-200"
     const GetProfileData = () => {
-        fetch("http://localhost:4000/owners/mango")
+        fetch("http://localhost:4000/owners/owner123")
             .then(res => res.json())
             .then(responseData => {
                 console.log(responseData)
@@ -23,14 +25,12 @@ function DashboardProfile() {
             <div className="flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row">
                 <div className="  mx-8 ">
                     <div className="my-8">
-                        <button onClick={() => setActive("account settings")} className="  flex 
-                        font-semibold  px-12 py-3 hover:border-blue-500
-                        hover:text-indigo-900 border rounded-2xl" >account settings</button>
+                        <button onClick={() => setActive("account settings")}
+                        className={`${buttonStyle} ${active === "account settings" ? activeStyle : "hover:text-indigo-900"}`} >Account Settings</button>
                     </div>
                     <div className="my-8">
-                        <button className=" flex 
-                        font-semibold  px-14 py-3
-                        hover:text-indigo-900 border border-grey-500 rounded-2xl hover:border-blue-500" onClick={() => setActive("space settings")}>space settings</button>
+                        <button className={`${buttonStyle} ${active === "space settings" ? activeStyle : "hover:text-indigo-900"}`} 
+                        onClick={() => setActive("space settings")}>Space Settings</button>
                     </div>
                 </div>
                 {active === "account settings" && <OwnerAccountSettings profileData={profileData}/>
