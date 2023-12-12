2aa564374b04ac0a7e12a596b5520d7236db09f7
amodather <abdo.modather10@gmail.com>
2023-12-12 12:02:31 +0200

    Added token to request's header

--- Front-end/src/components/reduxtoolkit/Slices/apiSlice.js
+++ Front-end/src/components/reduxtoolkit/Slices/apiSlice.js
@@ -4,7 +4,6 @@ import { setCredentials, logOut } from "./authSlice";
 
 const baseQuery = fetchBaseQuery({
     baseUrl: "http://localhost:4000",
-    credentials:"include",
     prepareHeaders: (headers, {getState}) =>{
         const token = getState().auth.token;
         if(token){
@@ -16,23 +15,40 @@ const baseQuery = fetchBaseQuery({
 
 const baseQueryWithReauth = async (args, api, extraOptions) =>{
     let result = await baseQuery(args, api, extraOptions)
-    if (result?.error?.originalStatus === 403) {
-        console.log('Sending refresh token')
+    console.log(result)
+    if (result?.error?.status === 401) {
+        console.log(result?.error)
+        // console.log('Sending refresh token')
         // Send refresh token to get new access token
-        const refreshResult = await baseQuery('/refresh' , api, extraOptions)
-        console.log(refreshResult)
-        if(refreshResult?.data){
-            const user = api.getState().auth.username
-            api.dispatch(setCredentials({...refreshResult , user}))
-            result = await baseQuery(args, api, extraOptions)
-        }else{
-            api.dispatch(logOut())
-        }
+        // const refreshResult = await baseQuery('/refresh' , api, extraOptions)
+        // console.log(refreshResult)
+        // if(refreshResult?.data){
+        //     const user = api.getState().auth.username
+        //     api.dispatch(setCredentials({...refreshResult , user}))
+        //     result = await baseQuery(args, api, extraOptions)
+        // }else{
+        //     api.dispatch(logOut())
+        // }
+        api.dispatch(logOut())
     }
     return result
 }
 
 export const apiSlice = createApi({
+    reducerPath:"api",
     baseQuery : baseQueryWithReauth,
-    endpoints: builder => ({})
+    endpoints: builder => ({
+        updateClient: builder.mutation({
+            query: credentials =>({
+                url: `/clients/${credentials.id}`,
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: credentials.credentials
+            })
+        })
+    })
 })
+
+export const {useUpdateClientMutation} = apiSlice;
