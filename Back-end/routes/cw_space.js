be2f996f38d651d86744d485e18f1c1e226eb804
Tarek-Ahmed-Helmy <tarek.ahmed35.01@gmail.com>
2023-12-16 21:05:51 +0200

    edit data getting for cwSpace

--- Back-end/routes/cw_space.js
+++ Back-end/routes/cw_space.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cw_spaceController = require('../controllers/cw_spaceController')
 const router = express.Router();
-const { validateCw_space , validateUpdatedCw_space} = require("../middlewares/validationSchema");
-
+const { validateCw_space, validateUpdatedCw_space} = require("../middlewares/validationSchema");
+const httpStatusCode = require("../utils/httpStatusText");
+const appError = require("../utils/appError");
 const multer = require('multer')
-const fs = require('fs')
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './public/images/cw_spaces')
@@ -17,14 +18,16 @@ const storage = multer.diskStorage({
             errors = validateCw_space(req)
         }
         if (errors.length != 0) {
-            return cb(new Error(errors.join(', ')), null);
+            const error = appError.create(errors, 415, httpStatusCode.ERROR)
+            return cb(error);
         }
         const acceptedFormats = ['image/png', 'image/jpeg', 'image/jpg'];
         if (!acceptedFormats.includes(file.mimetype)) {
-            return cb(new Error('Wrong file type'), null);
+            const error = appError.create("Unacceptable Type Format For Image", 415, httpStatusCode.ERROR)
+            return cb(error);
         }
         const uniqueSuffix = Date.now() + "." + file.originalname.split('.')[1];
-        req.body.imageName = uniqueSuffix
+        req.body.imageName = uniqueSuffix;
         cb(null, uniqueSuffix);
     }
 })
@@ -33,13 +36,16 @@ const upload = multer({ storage: storage })
 
 router.route("/home").get(cw_spaceController.getHome);
 
+router.route("/updatePhoto/:ID")
+    .patch(upload.single('mainPhoto'), cw_spaceController.updatePhoto);
+
 router.route("/")
-    .get(cw_spaceController.get)
+    .get(cw_spaceController.getAll)
     .post(upload.single('mainPhoto'), cw_spaceController.create);
 
 router.route("/:ID")
     .get(cw_spaceController.getOne)
-    .patch(upload.single('mainPhoto'), cw_spaceController.update)
+    .patch(cw_spaceController.update)
     .delete(cw_spaceController.delete);
 
 
