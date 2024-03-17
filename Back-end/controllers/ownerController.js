7399224c8c5533170f44ae56687b6149cc3ad99e
Tarek-Ahmed-Helmy <tarek.ahmed35.01@gmail.com>
2024-03-17 14:59:26 +0200

    unverified style

--- Back-end/controllers/ownerController.js
+++ Back-end/controllers/ownerController.js
@@ -133,7 +133,7 @@ module.exports = {
                 bcrypt.compare(enteredPassword, owner.password, async (err, result) => {
                     if (result) {
                         if (owner.verified === 0) {
-                            const error = appError.create(`This Email ${client.email} Not Verified`, 400, httpStatusCode.UNVERIFIED)
+                            const error = appError.create(client.email, 400, httpStatusCode.UNVERIFIED)
                             return next(error)
                         }
                         delete owner.verificationCode;
